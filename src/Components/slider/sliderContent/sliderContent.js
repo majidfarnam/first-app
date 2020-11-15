2451b53bf119e521d8ea44f1b1325ebb99652313
majid farnam <majidfarnam@outlook.com>
2020-11-16 00:51:00 +0330

    add link to slider Button

--- src/Components/slider/sliderContent/sliderContent.js
+++ src/Components/slider/sliderContent/sliderContent.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './sliderContent.css';
 import Button from '../../button/button';
 
+
 const SliderContent =()=>{
+
     return (
         <div className="sliderContent">
             <img src="http://www.pte90.net/wp-content/uploads/2020/10/1-1.jpg" alt="تست اسلاید" />
@@ -11,7 +13,11 @@ const SliderContent =()=>{
                 <h1>Focus on PTE Vocabulary</h1>
                 <h2>The best book for</h2>
                 <h2>reading PTE vocab</h2>
-                <Button btnType="danger">خرید کتاب</Button>
+                <a href="https://www.amazon.com/Focus-PTE-Vocabulary-Self-study-vocabulary-ebook-dp-B08HDN8B92/dp/B08HDN8B92/ref=mt_other?_encoding=UTF8&me=&qid=">
+                    <Button btnType="danger">خرید کتاب</Button>
+                </a>
+                    
+               
             </div>
             
         </div>
