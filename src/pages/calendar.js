f8c4c8e7aa336bc5c4af4d6ac9ff2ed803e52c03
majid farnam <majidfarnam@outlook.com>
2020-12-23 22:39:43 +0330

    add bootStrap and calendar to project

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,7 +1,12 @@
 import React, {Fragment, useState} from 'react';
-import Calendar from 'react-calendar';
 import '../Components/Calendar/calendar.css';
 import ToDo from "../Components/Calendar/toDo";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import 'react-calendar/dist/Calendar.css';
 
 const CalendarPage =(props)=>{
@@ -22,12 +27,23 @@ const CalendarPage =(props)=>{
             </div>
         <div className={'calendarContainer'} >
             <ToDo className={"toDoBox"} value={value}/>
-            <Calendar
+            {/* <Calendar
                 className={'calendar'}
                 onChange={onChange}
                 value={value}
-                />
+                /> */}
         </div>
+        <Container>
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
         </React.Fragment>
         
     )
